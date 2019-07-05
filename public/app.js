309957344ba4442abe1b4716d4a3efa41f4ff8f8
Drew Baugher <dbbaughe@amazon.com>
2019-07-05 11:41:32 -0700

    Adds initial Kibana plugin skeleton for Index Management

---
+++ public/app.js
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+import React from 'react';
+import { uiModules } from 'ui/modules';
+import chrome from 'ui/chrome';
+import { render, unmountComponentAtNode } from 'react-dom';
+import { I18nProvider } from '@kbn/i18n/react';
+
+import 'ui/autoload/styles';
+import { Main } from './components/main';
+
+const app = uiModules.get('apps/indexManagementKibana');
+
+app.config($locationProvider => {
+  $locationProvider.html5Mode({
+    enabled: false,
+    requireBase: false,
+    rewriteLinks: false,
+  });
+});
+app.config(stateManagementConfigProvider =>
+  stateManagementConfigProvider.disable()
+);
+
+function RootController($scope, $element, $http) {
+  const domNode = $element[0];
+
+  // render react to DOM
+  render(
+    <I18nProvider>
+      <Main title="index-management-kibana" httpClient={$http} />
+    </I18nProvider>,
+    domNode
+  );
+
+  // unmount react on controller destroy
+  $scope.$on('$destroy', () => {
+    unmountComponentAtNode(domNode);
+  });
+}
+
+chrome.setRootController('indexManagementKibana', RootController);
