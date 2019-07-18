1574624647527f00cef5e112ef7d792e339d0d4f
Drew Baugher <46505179+dbbaughe@users.noreply.github.com>
2019-07-18 14:01:58 -0700

    Adds Kibana ISM cluster (#4)

---
+++ server/utils/constants.ts
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+import { DefaultHeaders, IndexManagementApi } from "../models/interfaces";
+
+export const API_ROUTE_PREFIX = "/_opendistro/_ism";
+
+export const API: IndexManagementApi = {
+  POLICY_BASE: `${API_ROUTE_PREFIX}/policies`,
+  EXPLAIN_BASE: `${API_ROUTE_PREFIX}/explain`,
+  RETRY_BASE: `${API_ROUTE_PREFIX}/retry`,
+};
+
+export const DEFAULT_HEADERS: DefaultHeaders = {
+  "Content-Type": "application/json",
+  Accept: "application/json",
+};
+
+export enum CLUSTER {
+  ADMIN = "admin",
+  ISM = "opendistro_ism",
+  DATA = "data",
+}
+
+export enum INDEX {
+  OPENDISTRO_ISM_CONFIG = ".opendistro-ism-config",
+}
