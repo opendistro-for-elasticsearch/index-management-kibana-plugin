c3bbcd9397ba571ce83d09bb7c48467c1d71de4a
Drew Baugher <dbbaughe@amazon.com>
2019-07-27 22:27:29 -0700

    Adds Policies, interfaces, consatants, helpers, tests

---
+++ public/pages/Policies/containers/Policies/Policies.test.tsx
@@ -0,0 +1,283 @@
+/*
+ * Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+import React from "react";
+import "@testing-library/jest-dom/extend-expect";
+import { render, wait } from "@testing-library/react";
+// @ts-ignore
+import userEvent from "@testing-library/user-event";
+import { Redirect, Route, RouteComponentProps, Switch } from "react-router";
+import { MemoryRouter as Router } from "react-router-dom";
+import { toastNotifications } from "ui/notify";
+import chrome from "ui/chrome";
+import { browserServicesMock } from "../../../../../test/mocks";
+import Policies from "./Policies";
+import { TEXT } from "../../components/PolicyEmptyPrompt/PolicyEmptyPrompt";
+import { ModalProvider, ModalRoot } from "../../../../components/Modal";
+import { ServicesConsumer, ServicesContext } from "../../../../services";
+import { BREADCRUMBS } from "../../../../utils/constants";
+import { BrowserServices } from "../../../../models/interfaces";
+
+jest.mock("ui/notify", () => ({
+  toastNotifications: {
+    addDanger: jest.fn().mockName("addDanger"),
+    addSuccess: jest.fn().mockName("addSuccess"),
+  },
+}));
+
+jest.mock("ui/chrome", () => ({
+  breadcrumbs: (() => {
+    const breadcrumbs = () => {};
+    // @ts-ignore
+    breadcrumbs.set = jest.fn();
+    return breadcrumbs;
+  })(),
+}));
+
+// TODO: Move common renderWith or with___ helpers into top level tests directory
+function renderPoliciesWithRouter() {
+  return {
+    ...render(
+      <Router>
+        <ServicesContext.Provider value={browserServicesMock}>
+          <ServicesConsumer>
+            {(services: BrowserServices | null) =>
+              services && (
+                <ModalProvider>
+                  <ModalRoot services={services} />
+                  <Switch>
+                    <Route
+                      path="/policies"
+                      render={(props: RouteComponentProps) => (
+                        <div style={{ padding: "25px 25px" }}>
+                          <Policies {...props} policyService={services.policyService} />
+                        </div>
+                      )}
+                    />
+                    <Route path="/create-policy" render={props => <div>Testing create policy</div>} />
+                    <Route path="/edit-policy" render={props => <div>Testing edit policy: {props.location.search}</div>} />
+                    <Redirect from="/" to="/policies" />
+                  </Switch>
+                </ModalProvider>
+              )
+            }
+          </ServicesConsumer>
+        </ServicesContext.Provider>
+      </Router>
+    ),
+  };
+}
+
+// TODO: generate random policy (w/ seed for snapshots)
+const testPolicy = {
+  id: "some_policy_id",
+  primaryTerm: 1,
+  seqNo: 5,
+  policy: {
+    policy: {
+      description: "some policy description",
+      states: [
+        {
+          name: "some_state",
+          actions: [{ delete: {} }],
+          transitions: [],
+        },
+      ],
+    },
+  },
+};
+
+describe("<Policies /> spec", () => {
+  it("renders the component", async () => {
+    browserServicesMock.policyService.getPolicies = jest.fn().mockResolvedValue({ ok: true, response: { policies: [], totalPolicies: 0 } });
+    const { container } = renderPoliciesWithRouter();
+
+    expect(container.firstChild).toMatchSnapshot();
+  });
+
+  it("shows LOADING on mount", async () => {
+    browserServicesMock.policyService.getPolicies = jest.fn().mockResolvedValue({ ok: true, response: { policies: [], totalPolicies: 0 } });
+    const { getByText } = renderPoliciesWithRouter();
+
+    getByText(TEXT.LOADING);
+  });
+
+  it("sets breadcrumbs when mounting", async () => {
+    browserServicesMock.policyService.getPolicies = jest.fn().mockResolvedValue({ ok: true, response: { policies: [], totalPolicies: 0 } });
+    renderPoliciesWithRouter();
+
+    expect(chrome.breadcrumbs.set).toHaveBeenCalledTimes(1);
+    expect(chrome.breadcrumbs.set).toHaveBeenCalledWith([BREADCRUMBS.INDEX_MANAGEMENT, BREADCRUMBS.POLICIES]);
+  });
+
+  it("loads policies", async () => {
+    const policies = [testPolicy];
+    browserServicesMock.policyService.getPolicies = jest.fn().mockResolvedValue({ ok: true, response: { policies, totalPolicies: 1 } });
+    const { getByText } = renderPoliciesWithRouter();
+    await wait();
+
+    await wait(() => getByText(testPolicy.id));
+  });
+
+  it("adds error toaster when get policies has error", async () => {
+    browserServicesMock.policyService.getPolicies = jest.fn().mockResolvedValue({ ok: false, error: "some error" });
+    renderPoliciesWithRouter();
+
+    await wait();
+
+    expect(toastNotifications.addDanger).toHaveBeenCalledTimes(1);
+    expect(toastNotifications.addDanger).toHaveBeenCalledWith("some error");
+  });
+
+  it("adds error toaster when get policies throws error", async () => {
+    browserServicesMock.policyService.getPolicies = jest.fn().mockRejectedValue(new Error("rejected error"));
+    renderPoliciesWithRouter();
+
+    await wait();
+
+    expect(toastNotifications.addDanger).toHaveBeenCalledTimes(1);
+    expect(toastNotifications.addDanger).toHaveBeenCalledWith("rejected error");
+  });
+
+  it("can delete a policy", async () => {
+    const policies = [testPolicy];
+    browserServicesMock.policyService.getPolicies = jest
+      .fn()
+      .mockResolvedValueOnce({ ok: true, response: { policies, totalPolicies: 1 } })
+      .mockResolvedValueOnce({ ok: true, response: { policies: [], totalPolicies: 0 } });
+    browserServicesMock.policyService.deletePolicy = jest.fn().mockResolvedValue({ ok: true, response: true });
+    const { queryByText, getByText, getByTestId } = renderPoliciesWithRouter();
+
+    await wait(() => getByText(testPolicy.id));
+
+    expect(getByTestId("DeleteButton")).toBeDisabled();
+
+    userEvent.click(getByTestId(`checkboxSelectRow-${testPolicy.id}`));
+
+    expect(getByTestId("DeleteButton")).toBeEnabled();
+
+    userEvent.click(getByTestId("DeleteButton"));
+
+    await wait();
+
+    expect(browserServicesMock.policyService.deletePolicy).toHaveBeenCalledTimes(1);
+    expect(toastNotifications.addSuccess).toHaveBeenCalledTimes(1);
+    expect(toastNotifications.addSuccess).toHaveBeenCalledWith(`Deleted the policy: ${testPolicy.id}`);
+
+    await wait(() => expect(queryByText(testPolicy.id)).toBeNull());
+  });
+
+  it("can route to edit policy", async () => {
+    const policies = [testPolicy];
+    browserServicesMock.policyService.getPolicies = jest.fn().mockResolvedValue({ ok: true, response: { policies, totalPolicies: 1 } });
+    const { getByText, getByTestId } = renderPoliciesWithRouter();
+
+    await wait(() => getByText(testPolicy.id));
+
+    expect(getByTestId("EditButton")).toBeDisabled();
+
+    userEvent.click(getByTestId(`checkboxSelectRow-${testPolicy.id}`));
+
+    expect(getByTestId("EditButton")).toBeEnabled();
+
+    userEvent.click(getByTestId("EditButton"));
+
+    await wait(() => getByText(`Testing edit policy: ?id=${testPolicy.id}`));
+  });
+
+  it("can route to create policy", async () => {
+    browserServicesMock.policyService.getPolicies = jest.fn().mockResolvedValue({ ok: true, response: { policies: [], totalPolicies: 1 } });
+    const { getByText, getByTestId } = renderPoliciesWithRouter();
+
+    await wait();
+
+    userEvent.click(getByTestId("Create policyButton"));
+
+    await wait(() => getByText("Testing create policy"));
+  });
+
+  it("can open and close a policy in modal", async () => {
+    const policies = [testPolicy];
+    browserServicesMock.policyService.getPolicies = jest.fn().mockResolvedValue({ ok: true, response: { policies, totalPolicies: 1 } });
+    const { getByText, queryByText, getByTestId } = renderPoliciesWithRouter();
+
+    await wait(() => getByText(testPolicy.id));
+
+    userEvent.click(getByText(testPolicy.id));
+
+    // asserts that the policy description showed up in modal as the
+    // whole JSON is broken up between span elements
+    await wait(() => getByText(`"${testPolicy.policy.policy.description}"`));
+
+    userEvent.click(getByTestId("policyModalCloseButton"));
+
+    expect(queryByText(`"${testPolicy.policy.policy.description}"`)).toBeNull();
+  });
+
+  it("can go to edit a policy from modal", async () => {
+    const policies = [testPolicy];
+    browserServicesMock.policyService.getPolicies = jest.fn().mockResolvedValue({ ok: true, response: { policies, totalPolicies: 1 } });
+    const { getByText, getByTestId } = renderPoliciesWithRouter();
+
+    await wait(() => getByText(testPolicy.id));
+
+    userEvent.click(getByText(testPolicy.id));
+    userEvent.click(getByTestId("policyModalEditButton"));
+
+    await wait(() => getByText(`Testing edit policy: ?id=${testPolicy.id}`));
+  });
+
+  it("sorts/paginates the table", async () => {
+    const policies = new Array(40).fill(null).map((_, index) => ({
+      ...testPolicy,
+      id: `some_policy_id_${index}`,
+      // last_updated_time is effectively adding 1 to every policy so we can assert on the sort
+      policy: { policy: { ...testPolicy.policy.policy, last_updated_time: Date.now() + index } },
+    }));
+    browserServicesMock.policyService.getPolicies = jest
+      .fn()
+      .mockResolvedValueOnce({ ok: true, response: { policies: policies.slice(0, 20), totalPolicies: 40 } })
+      .mockResolvedValueOnce({ ok: true, response: { policies: policies.slice(20), totalPolicies: 40 } })
+      .mockResolvedValueOnce({
+        ok: true,
+        response: {
+          policies: policies
+            .slice()
+            .sort((a, b) => a.policy.policy.last_updated_time - b.policy.policy.last_updated_time)
+            .slice(0, 20),
+          totalPolicies: 40,
+        },
+      });
+
+    const { getByText, getByTestId, getAllByTestId, queryByText } = renderPoliciesWithRouter();
+
+    // should load policies 0-19 on first load
+    await wait(() => getByText("some_policy_id_0"));
+    expect(queryByText("some_policy_id_39")).toBeNull();
+
+    userEvent.click(getAllByTestId("pagination-button-next")[0]);
+
+    // should load policies 20-39 after clicking next
+    await wait(() => getByText("some_policy_id_39"));
+    expect(queryByText("some_policy_id_0")).toBeNull();
+
+    // @ts-ignore
+    userEvent.click(getByTestId("tableHeaderCell_policy.policy.last_updated_time_3").firstChild);
+
+    // should load policies 0-19  after clicking sort (defaults to asc) on last_updated_time
+    await wait(() => getByText("some_policy_id_0"));
+    expect(queryByText("some_policy_id_39")).toBeNull();
+  });
+});
---
+++ public/pages/Policies/containers/Policies/Policies.tsx
@@ -0,0 +1,304 @@
+/*
+ * Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+import React, { Component } from "react";
+import chrome from "ui/chrome";
+import { RouteComponentProps } from "react-router";
+import { toastNotifications } from "ui/notify";
+import queryString from "query-string";
+// @ts-ignore
+import { EuiBasicTable, EuiHorizontalRule, EuiLink } from "@elastic/eui";
+import _ from "lodash";
+import { ContentPanel, ContentPanelActions } from "../../../../components/ContentPanel";
+import PolicyControls from "../../components/PolicyControls";
+import PolicyEmptyPrompt from "../../components/PolicyEmptyPrompt";
+import PolicyModal from "../../../../components/PolicyModal";
+import { ModalConsumer } from "../../../../components/Modal";
+import { DEFAULT_PAGE_SIZE_OPTIONS, DEFAULT_QUERY_PARAMS } from "../../utils/constants";
+import { PoliciesQueryParams, PolicyItem } from "../../models/interfaces";
+import { getURLQueryParams, renderTime } from "../../utils/helpers";
+import { BREADCRUMBS, ROUTES, SortDirection } from "../../../../utils/constants";
+import { PolicyService } from "../../../../services";
+import { getErrorMessage } from "../../../../utils/helpers";
+import { TableParams } from "../../../../models/interfaces";
+
+interface PoliciesProps extends RouteComponentProps {
+  policyService: PolicyService;
+}
+
+interface PoliciesState {
+  totalPolicies: number;
+  from: number;
+  size: number;
+  search: string;
+  sortField: string;
+  sortDirection: SortDirection;
+  selectedItems: PolicyItem[];
+  policies: PolicyItem[];
+  loadingPolicies: boolean;
+}
+
+export default class Policies extends Component<PoliciesProps, PoliciesState> {
+  columns: object[];
+
+  constructor(props: PoliciesProps) {
+    super(props);
+
+    const { from, size, search, sortField, sortDirection } = getURLQueryParams(this.props.location);
+
+    this.state = {
+      totalPolicies: 0,
+      from,
+      size,
+      search,
+      sortField,
+      sortDirection,
+      selectedItems: [],
+      policies: [],
+      loadingPolicies: true,
+    };
+
+    this.getPolicies = _.debounce(this.getPolicies, 500, { leading: true });
+
+    this.columns = [
+      {
+        field: "id",
+        name: "Policy",
+        sortable: true,
+        truncateText: true,
+        textOnly: true,
+        width: "150px",
+        render: (name: string, item: PolicyItem) => (
+          <ModalConsumer>
+            {({ onShow, onClose }) => (
+              <EuiLink
+                onClick={() =>
+                  onShow(PolicyModal, { policyId: item.id, policy: item.policy, onEdit: () => this.onClickModalEdit(item, onClose) })
+                }
+              >
+                {name}
+              </EuiLink>
+            )}
+          </ModalConsumer>
+        ),
+      },
+      {
+        field: "policy.policy.description",
+        name: "Description",
+        sortable: true,
+        truncateText: true,
+        textOnly: true,
+        width: "150px",
+      },
+      {
+        field: "affectedIndices",
+        name: "Number of affected indices",
+        sortable: false,
+        truncateText: false,
+        width: "100px",
+        render: () => "#",
+      },
+      {
+        field: "policy.policy.last_updated_time",
+        name: "Last updated time",
+        sortable: true,
+        truncateText: false,
+        render: renderTime,
+        dataType: "date",
+        width: "150px",
+      },
+    ];
+  }
+
+  async componentDidMount() {
+    chrome.breadcrumbs.set([BREADCRUMBS.INDEX_MANAGEMENT, BREADCRUMBS.POLICIES]);
+    await this.getPolicies();
+  }
+
+  async componentDidUpdate(prevProps: PoliciesProps, prevState: PoliciesState) {
+    const prevQuery = Policies.getQueryObjectFromState(prevState);
+    const currQuery = Policies.getQueryObjectFromState(this.state);
+    if (!_.isEqual(prevQuery, currQuery)) {
+      await this.getPolicies();
+    }
+  }
+
+  static getQueryObjectFromState({ from, size, search, sortField, sortDirection }: PoliciesState): PoliciesQueryParams {
+    return { from, size, search, sortField, sortDirection };
+  }
+
+  getPolicies = async (): Promise<void> => {
+    this.setState({ loadingPolicies: true });
+    try {
+      const { policyService, history } = this.props;
+      const queryParamsString = queryString.stringify(Policies.getQueryObjectFromState(this.state));
+      history.replace({ ...this.props.location, search: queryParamsString });
+      const getPoliciesResponse = await policyService.getPolicies(queryParamsString);
+      if (getPoliciesResponse.ok) {
+        const {
+          response: { policies, totalPolicies },
+        } = getPoliciesResponse;
+        this.setState({ policies, totalPolicies });
+      } else {
+        toastNotifications.addDanger(getPoliciesResponse.error);
+      }
+    } catch (err) {
+      toastNotifications.addDanger(getErrorMessage(err, "There was a problem loading the policies"));
+    }
+    this.setState({ loadingPolicies: false });
+  };
+
+  deletePolicy = async (item: PolicyItem): Promise<boolean> => {
+    const { policyService } = this.props;
+    try {
+      const { id } = item;
+      const deletePolicyResponse = await policyService.deletePolicy(id);
+      if (deletePolicyResponse.ok) {
+        toastNotifications.addSuccess(`Deleted the policy: ${item.id}`);
+        return true;
+      } else {
+        toastNotifications.addDanger(`Failed to delete the policy, ${deletePolicyResponse.error}`);
+      }
+    } catch (err) {
+      toastNotifications.addDanger(getErrorMessage(err, "There was a problem deleting the policy"));
+    }
+    return false;
+  };
+
+  onTableChange = ({ page: tablePage, sort }: TableParams): void => {
+    const { index: page, size } = tablePage;
+    const { field: sortField, direction: sortDirection } = sort;
+    this.setState({ from: page * size, size, sortField, sortDirection });
+  };
+
+  onSelectionChange = (selectedItems: PolicyItem[]): void => {
+    this.setState({ selectedItems });
+  };
+
+  onSearchChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    this.setState({ from: 0, search: e.target.value });
+  };
+
+  onPageClick = (page: number): void => {
+    const { size } = this.state;
+    this.setState({ from: page * size });
+  };
+
+  resetFilters = (): void => {
+    this.setState({ search: DEFAULT_QUERY_PARAMS.search });
+  };
+
+  onClickEdit = (): void => {
+    const {
+      selectedItems: [{ id }],
+    } = this.state;
+    if (id) this.props.history.push(`${ROUTES.EDIT_POLICY}?id=${id}`);
+  };
+
+  onClickCreate = (): void => {
+    this.props.history.push(ROUTES.CREATE_POLICY);
+  };
+
+  onClickDelete = async (): Promise<void> => {
+    const { selectedItems } = this.state;
+    if (selectedItems.length !== 1) return;
+    const deleted = await this.deletePolicy(selectedItems[0]);
+    if (deleted) await this.getPolicies();
+  };
+
+  onClickModalEdit = (item: PolicyItem, onClose: () => void): void => {
+    onClose();
+    if (!item || !item.id) return;
+    this.props.history.push(`${ROUTES.EDIT_POLICY}?id=${item.id}`);
+  };
+
+  render() {
+    const { totalPolicies, from, size, search, sortField, sortDirection, selectedItems, policies, loadingPolicies } = this.state;
+
+    const filterIsApplied = !!search;
+    const page = Math.floor(from / size);
+
+    const pagination = {
+      pageIndex: page,
+      pageSize: size,
+      pageSizeOptions: DEFAULT_PAGE_SIZE_OPTIONS,
+      totalItemCount: totalPolicies,
+    };
+
+    const sorting = {
+      sort: {
+        direction: sortDirection,
+        field: sortField,
+      },
+    };
+
+    const selection = {
+      onSelectionChange: this.onSelectionChange,
+      selectableMessage: (selectable: boolean) => (selectable ? undefined : undefined),
+    };
+
+    const actions = [
+      {
+        text: "Delete",
+        buttonProps: {
+          disabled: selectedItems.length !== 1,
+          onClick: this.onClickDelete,
+        },
+      },
+      {
+        text: "Edit",
+        buttonProps: {
+          disabled: selectedItems.length !== 1,
+          onClick: this.onClickEdit,
+        },
+      },
+      {
+        text: "Create policy",
+        buttonProps: {
+          onClick: this.onClickCreate,
+        },
+      },
+    ];
+
+    return (
+      <ContentPanel actions={<ContentPanelActions actions={actions} />} bodyStyles={{ padding: "initial" }} title="Policies">
+        <PolicyControls
+          activePage={page}
+          pageCount={Math.ceil(totalPolicies / size) || 1}
+          search={search}
+          onSearchChange={this.onSearchChange}
+          onPageClick={this.onPageClick}
+          onRefresh={this.getPolicies}
+        />
+
+        <EuiHorizontalRule margin="xs" />
+
+        <EuiBasicTable
+          columns={this.columns}
+          isSelectable={true}
+          itemId="id"
+          items={policies}
+          noItemsMessage={
+            <PolicyEmptyPrompt filterIsApplied={filterIsApplied} loading={loadingPolicies} resetFilters={this.resetFilters} />
+          }
+          onChange={this.onTableChange}
+          pagination={pagination}
+          selection={selection}
+          sorting={sorting}
+        />
+      </ContentPanel>
+    );
+  }
+}
