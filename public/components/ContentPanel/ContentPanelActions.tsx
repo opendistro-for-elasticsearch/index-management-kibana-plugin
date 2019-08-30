815ffe5c2f92ed2fc9bb8a6d91865cf1b788ffed
Drew Baugher <46505179+dbbaughe@users.noreply.github.com>
2019-08-29 21:18:58 -0700

    Adds support for modal type actions in ContentPanelActions (#34)

--- public/components/ContentPanel/ContentPanelActions.tsx
+++ public/components/ContentPanel/ContentPanelActions.tsx
@@ -15,20 +15,46 @@
 
 import React from "react";
 import { EuiButton, EuiFlexGroup, EuiFlexItem } from "@elastic/eui";
+import { ModalConsumer } from "../Modal";
 
 interface ContentPanelActionsProps {
-  actions: { text: string; buttonProps?: object; flexItemProps?: object }[];
+  actions: {
+    text: string;
+    buttonProps?: object;
+    flexItemProps?: object;
+    modal?: {
+      onClickModal: (onShow: (component: any, props: object) => void) => () => void;
+    };
+  }[];
 }
 
 const ContentPanelActions: React.SFC<ContentPanelActionsProps> = ({ actions }) => (
   <EuiFlexGroup justifyContent="spaceBetween" alignItems="center">
-    {actions.map(({ text, buttonProps = {}, flexItemProps = {} }, index) => (
-      <EuiFlexItem {...flexItemProps} grow={false} key={index}>
+    {actions.map(({ text, buttonProps = {}, flexItemProps = {}, modal = null }, index) => {
+      let button = (
         <EuiButton {...buttonProps} data-test-subj={`${text}Button`}>
           {text}
         </EuiButton>
-      </EuiFlexItem>
-    ))}
+      );
+
+      if (modal) {
+        button = (
+          <ModalConsumer>
+            {({ onShow }) => (
+              <EuiButton {...buttonProps} onClick={modal.onClickModal(onShow)} data-test-subj={`${text}Button`}>
+                {text}
+              </EuiButton>
+            )}
+          </ModalConsumer>
+        );
+      }
+
+      return (
+        <EuiFlexItem {...flexItemProps} grow={false} key={index}>
+          {button}
+        </EuiFlexItem>
+      );
+    })}
   </EuiFlexGroup>
 );
 
