130f9b7cb6259988ffd44ce9435ebcb3dd74d738
Drew Baugher <dbbaughe@amazon.com>
2020-03-16 15:38:34 -0700

    Adds github actions workflow for cypress tests

---
+++ cypress/support/constants.js
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+export const API_ROUTE_PREFIX = "/_opendistro/_ism";
+
+export const INDEX = {
+  OPENDISTRO_ISM_CONFIG: ".opendistro-ism-config",
+};
+
+export const API = {
+  POLICY_BASE: `${API_ROUTE_PREFIX}/policies`,
+  EXPLAIN_BASE: `${API_ROUTE_PREFIX}/explain`,
+  RETRY_BASE: `${API_ROUTE_PREFIX}/retry`,
+  ADD_POLICY_BASE: `${API_ROUTE_PREFIX}/add`,
+  REMOVE_POLICY_BASE: `${API_ROUTE_PREFIX}/remove`,
+  CHANGE_POLICY_BASE: `${API_ROUTE_PREFIX}/change_policy`,
+};
+
+export const PLUGIN_NAME = "opendistro_index_management_kibana";
