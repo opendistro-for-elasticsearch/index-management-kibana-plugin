cfef0780d85f81b9dd57d4957d1ee25ff104c82b
Annie <ann3431@hotmail.com>
2020-09-28 16:56:47 -0700

    Modified timezone selection

--- public/pages/CreateRollup/components/Schedule/Schedule.tsx
+++ public/pages/CreateRollup/components/Schedule/Schedule.tsx
@@ -30,6 +30,7 @@ interface ScheduleState {
   startDate: Moment;
   timezone: number;
 }
+
 const radios = [
   {
     id: `no`,
@@ -42,28 +43,28 @@ const radios = [
 ];
 
 const timezones = [
-  { value: +12, text: "UTC +12" },
-  { value: +11, text: "UTC +11" },
-  { value: +10, text: "UTC +10" },
-  { value: +9, text: "UTC +9" },
-  { value: +8, text: "UTC +8" },
-  { value: +7, text: "UTC +7" },
-  { value: +6, text: "UTC +6" },
-  { value: +5, text: "UTC +5" },
-  { value: +4, text: "UTC +4" },
-  { value: +3, text: "UTC +3" },
-  { value: +2, text: "UTC +2" },
-  { value: +1, text: "UTC +1" },
+  { value: 12, text: "UTC +12" },
+  { value: 11, text: "UTC +11" },
+  { value: 10, text: "UTC +10" },
+  { value: 9, text: "UTC +9" },
+  { value: 8, text: "UTC +8" },
+  { value: 7, text: "UTC +7" },
+  { value: 6, text: "UTC +6" },
+  { value: 5, text: "UTC +5" },
+  { value: 4, text: "UTC +4" },
+  { value: 3, text: "UTC +3" },
+  { value: 2, text: "UTC +2" },
+  { value: 1, text: "UTC +1" },
   { value: 0, text: "UTC +0" },
-  { value: -1, text: "UTC -01" },
-  { value: -2, text: "UTC -02" },
-  { value: -3, text: "UTC -03" },
-  { value: -4, text: "UTC -04" },
-  { value: -5, text: "UTC -05" },
-  { value: -6, text: "UTC -06" },
-  { value: -7, text: "UTC -07" },
-  { value: -8, text: "UTC -08" },
-  { value: -9, text: "UTC -09" },
+  { value: -1, text: "UTC -1" },
+  { value: -2, text: "UTC -2" },
+  { value: -3, text: "UTC -3" },
+  { value: -4, text: "UTC -4" },
+  { value: -5, text: "UTC -5" },
+  { value: -6, text: "UTC -6" },
+  { value: -7, text: "UTC -7" },
+  { value: -8, text: "UTC -8" },
+  { value: -9, text: "UTC -9" },
   { value: -10, text: "UTC -10" },
   { value: -11, text: "UTC -11" },
   { value: -12, text: "UTC -12" },
@@ -90,22 +91,25 @@ export default class Schedule extends Component<ScheduleProps, ScheduleState> {
     this.setState({ radioIdSelected: optionId });
   };
 
-  // onChangeTimezone = ()
+  onChangeTimezone = (e: ChangeEvent<HTMLSelectElement>): void => {
+    this.setState({ timezone: e.target.value });
+  };
 
   handleDateChange = (date: Moment): void => {
     this.setState({ startDate: date });
   };
 
   render() {
+    const { checked, radioIdSelected, startDate, timezone } = this.state;
     return (
       <ContentPanel bodyStyles={{ padding: "initial" }} title="Schedule" titleSize="s">
         <div style={{ paddingLeft: "10px" }}>
-          <EuiCheckbox id="jobEnabledByDefault" label="Job enabled by default" checked={this.state.checked} onChange={this.onChangeCheck} />
+          <EuiCheckbox id="jobEnabledByDefault" label="Job enabled by default" checked={checked} onChange={this.onChangeCheck} />
           <EuiSpacer size="m" />
           <EuiFormRow label="Recurring job">
             <EuiRadioGroup
               options={radios}
-              idSelected={this.state.radioIdSelected}
+              idSelected={radioIdSelected}
               onChange={(id) => this.onChangeRadio(id)}
               name="recurringJob"
               // legend={{
@@ -116,16 +120,11 @@ export default class Schedule extends Component<ScheduleProps, ScheduleState> {
           <EuiSpacer size="m" />
           {/*TODO: Add invalid and error for date picker*/}
           <EuiFormRow label="Job starts on">
-            <EuiDatePicker showTimeSelect selected={this.state.startDate} onChange={this.handleDateChange} />
+            <EuiDatePicker showTimeSelect selected={startDate} onChange={this.handleDateChange} />
           </EuiFormRow>
           <EuiSpacer size="m" />
           <EuiFormRow label={"Timezone"}>
-            <EuiSelect
-              id="timezone"
-              options={timezones}
-              // value={value}
-              // onChange={onChangeTimezone}
-            />
+            <EuiSelect id="timezone" options={timezones} value={timezone} onChange={this.onChangeTimezone} />
           </EuiFormRow>
         </div>
       </ContentPanel>
