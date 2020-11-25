8575475ccf845284c99540d9b89f5984853f96fb
Annie <leeyun@amazon.com>
2020-11-25 13:27:23 -0800

    Testing wait-on

--- cypress/integration/policies_spec.js
+++ cypress/integration/policies_spec.js
@@ -23,9 +23,6 @@ describe("Policies", () => {
     // Set welcome screen tracking to false
     localStorage.setItem("home:welcome:show", "false");
 
-    // Wait for 5s to avoid 503 server unavailable error
-    cy.wait(5000);
-
     // Visit ISM Kibana
     cy.visit(`${Cypress.env("kibana")}/app/${PLUGIN_NAME}`);
 
