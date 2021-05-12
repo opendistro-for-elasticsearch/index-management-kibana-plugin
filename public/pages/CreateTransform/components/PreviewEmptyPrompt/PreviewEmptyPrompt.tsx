6cbcf473486cb2d81303b9d10f07d6c69aeae0f9
Annie <leeyun@amazon.com>
2021-05-12 14:39:07 -0700

    Update PreviewEmptyPrompt.tsx

--- public/pages/CreateTransform/components/PreviewEmptyPrompt/PreviewEmptyPrompt.tsx
+++ public/pages/CreateTransform/components/PreviewEmptyPrompt/PreviewEmptyPrompt.tsx
@@ -1,11 +1,15 @@
-import { EuiEmptyPrompt, EuiPanel } from "@elastic/eui";
+import { EuiEmptyPrompt, EuiPanel, EuiText } from "@elastic/eui";
 import React from "react";
 
 export default function PreviewEmptyPrompt() {
   return (
     <EuiPanel>
       <EuiEmptyPrompt
-        title={<h4> No fields selected</h4>}
+        title={
+          <EuiText size="m">
+            <h4> No fields selected</h4>
+          </EuiText>
+        }
         body={<p>From the table above, select a field you want to transform by clicking the “plus” button next to the field name</p>}
       />
     </EuiPanel>
