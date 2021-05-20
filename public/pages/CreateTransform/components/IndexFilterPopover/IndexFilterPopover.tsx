824ed48b59be000d1c76bf2c283b400d8e512496
Annie Lee <71157062+leeyun-amzn@users.noreply.github.com>
2021-05-20 13:50:14 -0500

    Text modification and parse date fields on create step 2 (#179)
    * Update Index filter popover text and clear aggList when source index is changed
    
    * Clean up code and add helptext to preview table
    
    * Modify target field name for histogram
    
    * Parse date histogram render in step 2 tables

--- public/pages/CreateTransform/components/IndexFilterPopover/IndexFilterPopover.tsx
+++ public/pages/CreateTransform/components/IndexFilterPopover/IndexFilterPopover.tsx
@@ -36,7 +36,13 @@ interface IndexFilterPopoverProps {
   closePopover: () => void;
 }
 
-export default function IndexFilterPopover({ sourceIndex, fields, sourceIndexFilter, onChangeSourceIndexFilter, closePopover }: IndexFilterPopoverProps) {
+export default function IndexFilterPopover({
+  sourceIndex,
+  fields,
+  sourceIndexFilter,
+  onChangeSourceIndexFilter,
+  closePopover,
+}: IndexFilterPopoverProps) {
   const [selectedField, setSelectedField] = useState("");
   const [selectedOperator, setSelectedOperator] = useState("");
   const [selectedValue, setSelectedValue] = useState("");
@@ -95,7 +101,7 @@ export default function IndexFilterPopover({ sourceIndex, fields, sourceIndexFil
 
   function customEditor() {
     return (
-      <EuiFormRow label="Elasticsearch Query DSL">
+      <EuiFormRow label="Custom query DSL">
         <EuiCodeEditor value={queryDsl} onChange={(string) => setQueryDsl(string)} mode="json" width="100%" height="250px" />
       </EuiFormRow>
     );
