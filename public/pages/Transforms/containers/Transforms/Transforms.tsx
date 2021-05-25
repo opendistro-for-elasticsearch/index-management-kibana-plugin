2919ba849af33507ae73ae46874776867a6a5def
Annie <leeyun@amazon.com>
2021-05-25 16:21:20 -0700

    Add refresh button

--- public/pages/Transforms/containers/Transforms/Transforms.tsx
+++ public/pages/Transforms/containers/Transforms/Transforms.tsx
@@ -248,6 +248,11 @@ export default class Transforms extends Component<TransformProps, TransformState
           <EuiFlexItem grow={false}>
             <EuiFlexGroup alignItems="center" gutterSize="s">
               <EuiFlexItem grow={false}>
+                <EuiButton iconType="refresh" onClick={this.getTransforms} data-test-subj="refreshButton">
+                  Refresh
+                </EuiButton>
+              </EuiFlexItem>
+              <EuiFlexItem grow={false}>
                 <EuiButton disabled={!selectedItems.length} onClick={this.onDisable} data-test-subj="disableButton">
                   Disable
                 </EuiButton>
@@ -351,7 +356,11 @@ export default class Transforms extends Component<TransformProps, TransformState
   };
 
   getSelectedTransformIds = () => {
-    return this.state.selectedItems.map((item: DocumentTransform) => { return item._id }).join(", ");
+    return this.state.selectedItems
+      .map((item: DocumentTransform) => {
+        return item._id;
+      })
+      .join(", ");
   };
 
   onSelectionChange = (selectedItems: DocumentTransform[]): void => {
